d76f6b581eba4d0ad2703025ebdeb3d67ff2f343
Gesiel Diniz da Silva <43421831+Gesiel-Diniz@users.noreply.github.com>
2023-02-24 18:49:54 -0300

    Start project

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.scss'
+
+const Footer = () => {
+  return <footer className="bg-light footer">
+    <div className="container">
+        
+    <p>H-FIPE &copy; 2023</p>
+    </div>
+  </footer>
+}
+
+export default Footer
